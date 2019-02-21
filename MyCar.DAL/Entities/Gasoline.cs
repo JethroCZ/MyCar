b3669dcd70aa6cf56a5a52f412b2bde414ad77a7
AJ <ales.jaterka@fullsys.cz>
2019-02-21 20:00:11 +0100

    Doplnění entit.

---
+++ MyCar.DAL/Entities/Gasoline.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyCar.DAL.Entities
+{
+    public class Gasoline
+    {
+        public int Id { get; set; }
+
+        public int CarId { get; set; }
+
+        public decimal Mileage { get; set; }
+
+        public decimal GasolineTanked { get; set; }
+
+        public DateTime? RefiledDate { get; set; }
+    }
+}
