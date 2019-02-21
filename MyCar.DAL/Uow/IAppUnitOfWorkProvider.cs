885b1afd4c69a4c19e8ebeec133b9c7df3347975
AJ <ales.jaterka@fullsys.cz>
2019-02-21 20:17:48 +0100

    Doplnění AppDbContextu a UnitOfWorku. Doplnění nugetů.

---
+++ MyCar.DAL/Uow/IAppUnitOfWorkProvider.cs
@@ -0,0 +1,8 @@
+﻿using Riganti.Utils.Infrastructure.EntityFrameworkCore;
+
+namespace MyCar.DAL.Uow
+{
+    public interface IAppUnitOfWorkProvider : IEntityFrameworkUnitOfWorkProvider<AppDbContext>
+    {
+    }
+}
