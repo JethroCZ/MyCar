73865d285c0e2baaea33fc9ce76f18ea87e89891
AJ <ales.jaterka@fullsys.cz>
2019-03-03 14:54:28 +0100

    Doplnění aplikačního repozitáře.

---
+++ MyCar.BL/Repositories/AppRepository.cs
@@ -0,0 +1,16 @@
+﻿using MyCar.DAL;
+using MyCar.DAL.Uow;
+using Riganti.Utils.Infrastructure.Core;
+using Riganti.Utils.Infrastructure.EntityFrameworkCore;
+
+namespace MyCar.BL.Repositories
+{
+    public class AppRepository<TEntity, TKey> : EntityFrameworkRepository<TEntity, TKey, AppDbContext>, IAppRepository<TEntity, TKey>
+    where TEntity : class, IEntity<TKey>, new()
+    {
+        public AppRepository(IAppUnitOfWorkProvider unitOfWorkProvider, IDateTimeProvider dateTimeProvider)
+            : base(unitOfWorkProvider, dateTimeProvider)
+        {
+        }
+    }
+}
---
+++ MyCar.BL/Repositories/IAppRepository.cs
@@ -0,0 +1,12 @@
+﻿using MyCar.DAL;
+using Riganti.Utils.Infrastructure.Core;
+using Riganti.Utils.Infrastructure.EntityFrameworkCore;
+
+namespace MyCar.BL.Repositories
+{
+    public interface IAppRepository<TEntity, in TKey> : IEntityFrameworkRepository<TEntity, TKey, AppDbContext>
+        where TEntity : IEntity<TKey>
+    {
+        
+    }
+}
