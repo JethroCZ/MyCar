abbfe3d530cd542484d539e0fdbde46ab1804b49
AJ <ales.jaterka@fullsys.cz>
2019-03-04 06:54:16 +0100

    Doplnění CarControlleru a view pro Vytvoření nového záznamu.

---
+++ MyCar/Controllers/CarController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCar.MVC.Controllers
+{
+    public class CarController : Controller
+    {
+        // GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
